728145add112f358c1bd869295ca19cd9b086b04
Yuna Lee <shha0213@gmail.com>
2024-02-02 12:47:41 +0900

    프로젝트 초기 설정

---
+++ src/test/java/com/hanghae/newsfeed/NewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
