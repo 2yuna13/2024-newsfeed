2df1d672674f5fcbd2643374fbb2194dbf8cfc85
Yuna Lee <shha0213@gmail.com>
2024-02-02 23:44:59 +0900

    [feat/user] 유저 정보 수정 구현

---
+++ src/main/java/com/hanghae/newsfeed/user/dto/response/UserResponseDto.java
@@ -0,0 +1,29 @@
+package com.hanghae.newsfeed.user.dto.response;
+
+import com.hanghae.newsfeed.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String email;
+    private String nickname;
+    private String description;
+    private String profileImage;
+    private String msg;
+
+    public static UserResponseDto createUserDto(User user, String msg) {
+        return new UserResponseDto(
+                user.getId(),
+                user.getEmail(),
+                user.getNickname(),
+                user.getDescription(),
+                user.getProfileImage(),
+                msg
+        );
+    }
+}
--- src/main/java/com/hanghae/newsfeed/user/service/UserService.java
+++ src/main/java/com/hanghae/newsfeed/user/service/UserService.java
@@ -2,8 +2,10 @@ package com.hanghae.newsfeed.user.service;
 
 import com.hanghae.newsfeed.user.dto.request.LoginRequestDto;
 import com.hanghae.newsfeed.user.dto.request.SignupRequestDto;
+import com.hanghae.newsfeed.user.dto.request.UserRequestDto;
 import com.hanghae.newsfeed.user.dto.response.LoginResponseDto;
 import com.hanghae.newsfeed.user.dto.response.SignupResponseDto;
+import com.hanghae.newsfeed.user.dto.response.UserResponseDto;
 import com.hanghae.newsfeed.user.entity.User;
 import com.hanghae.newsfeed.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -43,7 +45,7 @@ public class UserService {
         User createdUser = userRepository.save(user);
 
         // DTO로 변경하여 반환
-        return new SignupResponseDto(createdUser.getId(), createdUser.getEmail(), createdUser.getNickname());
+        return SignupResponseDto.createUserDto(createdUser, "회원가입 성공");
     }
 
     public LoginResponseDto login(LoginRequestDto requestDto) {
@@ -58,6 +60,28 @@ public class UserService {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
         }
 
-        return new LoginResponseDto(user.getEmail());
+        return new LoginResponseDto("로그인 성공");
+    }
+
+    @Transactional
+    public UserResponseDto update(Long id, UserRequestDto requestDto) {
+        // 유저 조회 예외 발생
+        User target = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("유저 정보 수정 실패, 등록된 사용자가 없습니다."));
+        log.info("서비스" + target);
+
+        // 닉네임 중복 확인
+        userRepository.findByNickname(requestDto.getNickname()).ifPresent(user -> {
+            throw new IllegalArgumentException("중복된 닉네임이 존재합니다.");
+        });
+
+        // 유저 수정
+        target.patch(requestDto);
+
+        // DB로 갱신
+        User updatedUser = userRepository.save(target);
+
+        // DTO로 변경하여 반환
+        return UserResponseDto.createUserDto(updatedUser, "유저 정보 수정 성공");
     }
 }
