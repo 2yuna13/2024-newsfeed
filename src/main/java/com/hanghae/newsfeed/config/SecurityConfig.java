540b519736ca6d989e95536918c9ee93d1ab3949
Yuna Lee <shha0213@gmail.com>
2024-02-04 01:18:39 +0900

    [feat/auth] jwt 기능 구현

---
+++ src/main/java/com/hanghae/newsfeed/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.hanghae.newsfeed.config;
+
+import com.hanghae.newsfeed.security.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        // 회원가입 및 로그인은 모든 사용자에게 허용
+                        .requestMatchers(HttpMethod.POST, "/api/auth/signup").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        // 게시물 및 댓글 조회는 모든 사용자에게 허용
+                        .requestMatchers(HttpMethod.GET, "/api/posts").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/comments").permitAll()
+                        // 나머지 요청은 인증된 사용자에게만 허용
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/hanghae/newsfeed/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,79 @@
+package com.hanghae.newsfeed.security.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider { // 토큰을 만들고 분석하는 클래스
+    @Value("${jwt.secret-key.access}")
+    private String accessSecretKey;
+    @Value("${jwt.secret-key.refresh}")
+    private String refreshSecretKey;
+    private final Long ACCESS_TOKEN_EXPIRE_TIME = 30 * 60 * 1000L;
+    private final Long REFRESH_TOKEN_EXPIRE_TIME = 60 * 60 * 24 * 14 * 1000L;
+
+    // 토큰 생성
+    public String generate(String email, JwtTokenType type){
+        Claims claims = Jwts.claims();
+        claims.put("email", email);
+//        claims.put("userRole", role); // 사용자 권한을 클레임에 추가
+
+        Date date = new Date();
+
+        String secretKey;
+        Long expiredTime;
+        if (type.equals(JwtTokenType.ACCESS)) {
+            secretKey = accessSecretKey;
+            expiredTime = ACCESS_TOKEN_EXPIRE_TIME;
+        } else {
+            secretKey = refreshSecretKey;
+            expiredTime = REFRESH_TOKEN_EXPIRE_TIME;
+        }
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(date)
+                .setExpiration(new Date(date.getTime() + expiredTime))
+                .signWith(getKey(secretKey), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // 토큰 만료 확인
+    public boolean isExpired(String token, JwtTokenType type) {
+        Date expiredDate = extractClaims(token, type).getExpiration();
+        return expiredDate.before(new Date());
+    }
+
+    // 만료 시간 확인
+    public long getExpiredTime(String token, JwtTokenType type) {
+        Date expiredDate = extractClaims(token, type).getExpiration();
+        Date currentDate = new Date();
+        return expiredDate.getTime() - currentDate.getTime();
+    }
+
+    // 토큰에서 사용자 정보 가져오기
+    public String getEmail(String token, JwtTokenType type) {
+        return extractClaims(token, type).get("email", String.class);
+    }
+
+    // claims 추출하는 메소드
+    private Claims extractClaims(String token, JwtTokenType type) {
+        if (type.equals(JwtTokenType.ACCESS)) {
+            return Jwts.parserBuilder().setSigningKey(getKey(accessSecretKey))
+                    .build().parseClaimsJws(token).getBody();
+        }
+        return Jwts.parserBuilder().setSigningKey(getKey(refreshSecretKey))
+                .build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getKey(String key) {
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
