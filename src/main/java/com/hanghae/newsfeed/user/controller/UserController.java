91cdeb2d9cd1b13c0d68292c615978dfa654d206
Yuna Lee <shha0213@gmail.com>
2024-02-04 12:01:04 +0900

    [fix/user] 유저 정보 수정에서 받아오는 값 변경

--- src/main/java/com/hanghae/newsfeed/user/controller/UserController.java
+++ src/main/java/com/hanghae/newsfeed/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.hanghae.newsfeed.user.controller;
 
+import com.hanghae.newsfeed.security.UserDetailsImpl;
 import com.hanghae.newsfeed.user.dto.request.UserRequestDto;
 import com.hanghae.newsfeed.user.dto.response.UserResponseDto;
 import com.hanghae.newsfeed.user.service.UserService;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -18,8 +20,13 @@ public class UserController {
     private final UserService userService;
 
     // 회원 정보 수정
-    @PatchMapping("/{id}")
-    public ResponseEntity<UserResponseDto> update(@PathVariable Long id, @RequestBody @Valid UserRequestDto requestDto) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.update(id, requestDto));
+    @PatchMapping
+    public ResponseEntity<UserResponseDto> update(
+            @AuthenticationPrincipal final UserDetailsImpl userDetails,
+            @RequestBody @Valid UserRequestDto requestDto
+    ) {
+        requestDto.setId(userDetails.getId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.update(requestDto));
     }
 }
