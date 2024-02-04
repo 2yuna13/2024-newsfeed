966773ca4d54854430916fe85250dc6c8498fb93
Yuna Lee <shha0213@gmail.com>
2024-02-04 12:01:27 +0900

    [feature/post] 게시물 수정, 삭제 기능 구현

--- src/main/java/com/hanghae/newsfeed/post/controller/PostController.java
+++ src/main/java/com/hanghae/newsfeed/post/controller/PostController.java
@@ -3,10 +3,12 @@ package com.hanghae.newsfeed.post.controller;
 import com.hanghae.newsfeed.post.dto.request.PostRequestDto;
 import com.hanghae.newsfeed.post.dto.response.PostResponseDto;
 import com.hanghae.newsfeed.post.service.PostService;
+import com.hanghae.newsfeed.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -18,9 +20,27 @@ public class PostController {
 
     // 게시물 작성
     @PostMapping
-    public ResponseEntity<PostResponseDto> create(@RequestBody PostRequestDto requestDto) {
-        requestDto.setUserId(1L); // 로그인 후 정보 받아오기 구현해야함.
+    public ResponseEntity<PostResponseDto> create(
+            @AuthenticationPrincipal final UserDetailsImpl userDetails,
+            @RequestBody PostRequestDto requestDto
+    ) {
+        requestDto.setUserId(userDetails.getId());
 
         return ResponseEntity.status(HttpStatus.OK).body(postService.create(requestDto));
+    }
+
+    // 게시물 수정
+    @PatchMapping("/{id}")
+    public ResponseEntity<PostResponseDto> update(
+            @PathVariable Long id,
+            @RequestBody PostRequestDto requestDto
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(postService.update(id, requestDto));
+    }
+
+    // 게시물 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PostResponseDto> delete(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(postService.delete(id));
     }
 }
