7520f6d5ccbf4e94bb67e064d2ba296054cdb6c5
Yuna Lee <shha0213@gmail.com>
2024-02-08 22:52:29 +0900

    [feat/admin] 관리자 회원 권한 수정/삭제 기능 구현

--- src/main/java/com/hanghae/newsfeed/admin/controller/AdminController.java
+++ src/main/java/com/hanghae/newsfeed/admin/controller/AdminController.java
@@ -1,13 +1,12 @@
 package com.hanghae.newsfeed.admin.controller;
 
+import com.hanghae.newsfeed.user.dto.request.UserRequestDto;
 import com.hanghae.newsfeed.user.dto.response.UserResponseDto;
 import com.hanghae.newsfeed.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +20,16 @@ public class AdminController {
     @GetMapping("/users")
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+    }
+
+    // 회원 권한 수정(USER -> ADMIN / active = false)
+    @PatchMapping("/{userId}")
+    public ResponseEntity<UserResponseDto> updateUserRoleAndStatus(
+            @PathVariable Long userId,
+            @RequestBody UserRequestDto requestDto
+    ) {
+        requestDto.setId(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUserRoleAndStatus(requestDto));
     }
 }
