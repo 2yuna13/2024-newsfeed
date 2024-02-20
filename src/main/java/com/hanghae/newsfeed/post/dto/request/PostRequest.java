bda1fefa55732e399d26feb68d4162bc39d356c6
Yuna Lee <shha0213@gmail.com>
2024-02-20 12:43:27 +0900

    [feat/aop] aop 입출력 로깅 추가

--- src/main/java/com/hanghae/newsfeed/post/dto/request/PostRequest.java
+++ src/main/java/com/hanghae/newsfeed/post/dto/request/PostRequest.java
@@ -3,9 +3,11 @@ package com.hanghae.newsfeed.post.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class PostRequest {
     @NotBlank
     private String title;
--- src/main/java/com/hanghae/newsfeed/post/dto/response/MultimediaResponse.java
+++ src/main/java/com/hanghae/newsfeed/post/dto/response/MultimediaResponse.java
@@ -2,8 +2,10 @@ package com.hanghae.newsfeed.post.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 @AllArgsConstructor
 public class MultimediaResponse {
     private String fileUrl;
