9c424bdcc08249922daf3854a12a4ed979c5b745
Yuna Lee <shha0213@gmail.com>
2024-02-24 03:04:21 +0900

    [test/admin] 관리자 관련 테스트 코드 작성

--- src/main/java/com/hanghae/newsfeed/admin/dto/request/AdminUserRequest.java
+++ src/main/java/com/hanghae/newsfeed/admin/dto/request/AdminUserRequest.java
@@ -3,11 +3,13 @@ package com.hanghae.newsfeed.admin.dto.request;
 import com.hanghae.newsfeed.user.type.UserRoleEnum;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
+@Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminUserRequest {
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
